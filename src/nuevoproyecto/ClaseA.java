f23051b8517cbd7bb066a0d33ec4750e679e9175
lll <dam101@PRO101.inforjm.local>
2017-04-27 12:15:57 +0200

    Commit de Jesus 1

---
+++ src/nuevoproyecto/ClaseA.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevoproyecto;
+
+/**
+ *
+ * @author dam101
+ */
+public class ClaseA {
+    private String amigo;
+
+    public ClaseA(String amigo) {
+        this.amigo = amigo;
+    }
+    
+}
