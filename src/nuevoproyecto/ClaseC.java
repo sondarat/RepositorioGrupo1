383eff42204e913323326365414e9008459d71b1
ismael <ismael.moreno>
2017-04-27 12:16:34 +0200

    este commit es la bomba

---
+++ src/nuevoproyecto/ClaseC.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevoproyecto;
+
+/**
+ *
+ * @author dam103
+ */
+public class ClaseC {
+    private String zipote;
+ // acompañame una noche mas...   
+
+    public String getZipote() {
+        return zipote;
+    }
+    
+}
